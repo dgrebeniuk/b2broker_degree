2e03578b55bb7d612ac01a5c5d10ccc5442a1afe
Dmitrii Grebeniuk <dmtrgrbnk@gmail.com>
2023-05-21 18:10:04 +0300

    added tests

--- src/test/java/tests/MainPageTest.java
+++ src/test/java/tests/MainPageTest.java
@@ -1,38 +1,38 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.value;
 import static com.codeborne.selenide.Selenide.*;
 
 public class MainPageTest {
 
-   @BeforeAll
-   static void beforeAll() {
+   @BeforeEach
+   void beforeEach() {
       Configuration.browserSize = "1440x812";
       open("https://stand-qa-06-my.b2broker.tech");
    }
-
 
    @Test
    @DisplayName("Открывается страница регистрации")
    void openRegistrationPage() {
       $(".mat-button-wrapper").click();
-      sleep(5000);
+      $(".registration-type-title").shouldHave(text("Choose Registration Type"));
    }
 
    @Test
    @DisplayName("Отображается QR-code для авторизации")
    void displayQrCode() {
-
+      $(".qr-code-block").shouldHave(text("QR Code Login"));
    }
 
    @Test
    @DisplayName("Открывается страница для восстановления пароля")
    void openResetPassword() {
-
+      $(".restore-link").click();
+      $(".ng-star-inserted").shouldHave(text("Restore a password"));
    }
 
    @Test
@@ -44,7 +44,9 @@ public class MainPageTest {
    @Test
    @DisplayName("Отображение пароля")
    void displayPassword() {
-
+      $("#mat-input-1").setValue("123456As");
+      $(".toggle-password-button").click();
+      $("#mat-input-1").shouldHave(value("123456As"));
    }
 
    @Test

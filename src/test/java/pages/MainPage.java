39dec3c06b482d8da2aad1988f56c5accf59a925
Dmitrii Grebeniuk <dmtrgrbnk@gmail.com>
2023-05-23 20:38:01 +0300

    edited MainPage.java

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -1,5 +1,7 @@
 package pages;
 
+import com.codeborne.selenide.SelenideElement;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.value;
 import static com.codeborne.selenide.Selectors.byText;
@@ -8,6 +10,22 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class MainPage {
 
+   private final SelenideElement
+           buttonRegistration = $(".mat-button-wrapper"),
+           registrationTitle = $(".registration-type-title"),
+           codeQR = $(".qr-code-block"),
+           restoreLink = $(".restore-link"),
+           restorePageTitle = $(".ng-star-inserted"),
+           userEmail = $("#mat-input-0"),
+           userPassword = $("#mat-input-1"),
+           signInButton = $($(".ng-star-inserted > .mat-focus-indicator.mat-flat-button.mat-button-base.mat-accent")),
+           pointMainPage = $(byText("Dashboard")),
+           showPasswordButton = $(".toggle-password-button"),
+           languageChangeButton = $("#mat-select-value-1"),
+           languageRU = $("#mat-option-1"),
+           textRU = $(".mat-button-wrapper"),
+           supportEmail = $(".footer");
+
    public MainPage openMainPage(){
       open("https://stand-qa-06-my.b2broker.tech");
 
@@ -15,91 +33,91 @@ public class MainPage {
    }
 
    public MainPage openRegistrationPage() {
-      $(".mat-button-wrapper").click();
+      buttonRegistration.click();
 
       return this;
    }
 
    public MainPage verifyOpenRegistrationPage(String value) {
-      $(".registration-type-title").shouldHave(text(value));
+      registrationTitle.shouldHave(text(value));
 
       return this;
    }
 
    public MainPage verifyDisplayQrCode(String value) {
-      $(".qr-code-block").shouldHave(text(value));
+      codeQR.shouldHave(text(value));
 
       return this;
    }
 
    public MainPage clickOnTheForgotYourPassword() {
-      $(".restore-link").click();
+      restoreLink.click();
 
       return this;
    }
 
    public MainPage verifyOpenRestorePage(String value) {
-      $(".ng-star-inserted").shouldHave(text(value));
+      restorePageTitle.shouldHave(text(value));
 
       return this;
    }
 
    public MainPage setUserEmail(String mail) {
-      $("#mat-input-0").setValue(mail);
+      userEmail.setValue(mail);
 
       return this;
    }
 
    public MainPage setUserPassword(String password) {
-      $("#mat-input-1").setValue(password);
+      userPassword.setValue(password);
 
       return this;
    }
 
    public MainPage clickSignInButton() {
-      $(".ng-star-inserted > .mat-focus-indicator.mat-flat-button.mat-button-base.mat-accent").click();
+      signInButton.click();
 
       return this;
    }
 
    public MainPage verifySignInTR() {
-      $(byText("Dashboard")).shouldHave(text("Dashboard"));
+      pointMainPage.shouldHave(text("Dashboard"));
 
       return this;
    }
 
    public MainPage clickOnTheShowPasswordButton() {
-      $(".toggle-password-button").click();
+      showPasswordButton.click();
 
       return this;
    }
 
    public MainPage verifyPasswordButton(String password) {
-      $("#mat-input-1").shouldHave(value(password));
+      userPassword.shouldHave(value(password));
 
       return this;
    }
 
    public MainPage clickOnTheLanguageChangeButton() {
-      $("#mat-select-value-1").click();
+      languageChangeButton.click();
 
       return this;
    }
 
    public MainPage clickOnTheLanguage() {
-      $("#mat-option-1").click();
+      languageRU.click();
 
       return this;
    }
 
    public MainPage verifyTheLanguageChange(String value) {
-      $(".mat-button-wrapper").shouldHave(text(value));
+      textRU.shouldHave(text(value));
 
       return this;
    }
 
    public MainPage verifyDisplaySupportMail(String value) {
-      $(".footer").shouldHave(text(value));
+      supportEmail.shouldHave(text(value));
 
       return this;
    }

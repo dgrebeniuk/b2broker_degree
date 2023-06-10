b5e0332a9968fed2c188d6ea987b726cbc99d3ac
Dmitrii Grebeniuk <dmtrgrbnk@gmail.com>
2023-06-10 10:37:53 +0300

    v1

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -17,6 +17,7 @@ public class TestBase {
    @BeforeAll
    static void beforeAll() {
       Configuration.remote = System.getProperty("remoteUrl", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
+      Configuration.baseUrl = System.getProperty("baseUrl", "https://stand-qa-06-my.b2broker.tech/");
       Configuration.browser = System.getProperty("browser", "chrome");
       Configuration.browserVersion = System.getProperty("browserVersion", "100");
       Configuration.browserSize = System.getProperty("browserSize", "1440x812");
